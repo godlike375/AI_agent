f1c18d931c81c94185f18f7248f5eb6018b1778e
Илья Туманов <ilya.tumanov@playrix.com>
2022-07-18 00:22:29 +0300

    Добавил тип объектов мира и проработал интерфейс создания мира

--- simulation.py
+++ simulation.py
@@ -4,9 +4,10 @@
 последствий действий learn(t) = learn(t-2)*action(t-2) -> learn(t-1)
 
 """
-from world import World
+from world import World, Agent
 from brain import Brain
 from base import Tickable
+from model import Model
 
 class Simulation(Tickable):
     """
@@ -14,10 +15,12 @@ class Simulation(Tickable):
     многоагентный мир
     """
     # TODO: 1 экземляр симуляции работает в 1 процессе из multiprocessing
-    def __init__(self, world: World, brains: list[Brain]):
+    def __init__(self, world, brains):
         self._world = world
         self._brains = brains
+        for agent in self._world.agents:
+            agent.link_brain(Brain(agent.inputs_count, agent.outputs_count, Model))
 
     def start(self):
-        for brain in self._brains:
-            brain.start()
+        while True:
+            self._world.tick()
--- world.py
+++ world.py
@@ -1,4 +1,6 @@
+import threading
 from base import Tickable
+
 
 class World(Tickable):
     """
@@ -7,20 +9,31 @@ class World(Tickable):
     чтобы он не сдох
     Мир задаёт тело, в которое будет помещен мозг. Соответственно, количество входов и выходов агента определяется миром
     """
-    # TODO: мир работает в потоке из threading, агент тоже
+    # TODO: мир работает в потоке из threading, агенты и объекты обрабатываются в этом же потоке
     def __init__(self, world):
-        self._world = world
+        self.agents = []
+        self.objects = []
         # TODO: подумать, что из себя представляет мир в виде данных
         pass
 
-class Agent:
+    def tick(self):
+        for agent in self._agents:
+            agent.tick()
+        for object in self._objects:
+            object.tick()
+
+
+class Agent(Tickable):
     """
     Базовый тип для агентов любого мира. На основе него создаются "тела" для мозга, который ничего не должен знать о мире
     """
-    def __init__(self, inputs_count, outputs_count, Brain_cls):
+    def __init__(self, inputs_count, outputs_count, world):
         self._inputs_count = inputs_count
         self._outputs_count = outputs_count
-        self._brain = Brain_cls(inputs_count, outputs_count)
+        self._world = world
+
+    def link_brain(self, brain):
+        self._brain = brain
 
     @property
     def inputs_count(self):
@@ -29,3 +42,27 @@ class Agent:
     @property
     def outputs_count(self):
         return self._outputs_count
+
+    def tick(self):
+        self._brain.inputs = self.get_inputs()
+        self.act(self.decode(self._brain.outputs))
+
+    def decode(self, outputs):
+        # Здесь должна быть расшифровка сигналов от мозга
+        pass
+
+    def act(self, decoded):
+        # Здесь выбор действий с миром в зависимости от расшифрованных сигналов
+        pass
+
+
+class Object(Tickable):
+    """
+    Базовый тип для объектов мира. Объект - набор свойств (полей)
+    """
+    def __init__(self, type, props):
+        self.type = type
+        self.properties = props or {}  # мб обычный лист
+
+    def tick(self):
+        pass
